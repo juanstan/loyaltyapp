e153712349ea2185abcd1d2d5aafef4a30f42f3f
juanstan <juanstan@gmail.com>
2022-06-26 23:09:28 +0100

    working on the register form

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import {AccountService} from '../../providers/account.service';
 import {AlertService} from '../../shared/services/alert.service';
-
+import * as moment from 'moment';
+import {IonDatetime} from '@ionic/angular';
+import {CountryService} from '../../providers/country.service';
+import {Observable} from 'rxjs/internal/Observable';
+import {Country} from '../../model/country';
+import {RegionService} from '../../providers/region.service';
+import {Region} from '../../model/region';
+import {CityService} from '../../providers/city.service';
+import {City} from '../../model/city';
 
 @Component({
   templateUrl: 'signup.html',
@@ -14,13 +22,21 @@ export class SignupComponent implements OnInit {
   form: FormGroup;
   loading = false;
   submitted = false;
+  date_of_birth: Date;
+  todayDate: string;
+  countries$: Observable<Country[]>;
+  regions$: Observable<Region[]>;
+  cities$: Observable<City[]>;
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private accountService: AccountService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private countryService: CountryService,
+    private regionService: RegionService,
+    private cityService: CityService
   ) { }
 
   ngOnInit() {
@@ -29,13 +45,16 @@ export class SignupComponent implements OnInit {
       last_name: ['', Validators.required],
       phone: ['', Validators.required],
       email: ['', Validators.required],
-      dob: ['', Validators.required],
+      gender: ['', Validators.required],
       country: ['', Validators.required],
       region: ['', Validators.required],
       city: ['', Validators.required],
+      date_of_birth: ['', Validators.required],
       password_confirmation: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+
+    this.getCountries();
   }
 
   // convenience getter for easy access to form fields
@@ -65,5 +84,68 @@ export class SignupComponent implements OnInit {
           this.loading = false;
         }
       });
+  }
+
+  getToday() {
+    return new Date();
+  }
+
+  getTodayUTC() {
+    return new Date().toISOString();
+  }
+
+
+  getData(data) {
+    this.form.patchValue({
+      date_of_birth: data.detail.value,
+    });
+  }
+
+  getCountries() {
+    this.countries$ = this.countryService.getCountriesReq();
+  }
+
+  loadRegion(event) {
+    const countryID = event?.detail?.value;
+    if (countryID) {
+      this.regions$ = this.regionService.getRegionsReq(countryID);
+    }
+  }
+
+  loadCity(event) {
+    const cityID = event?.detail?.value;
+    if (cityID) {
+      this.cities$ = this.cityService.getCitysReq(cityID);
+    }
+  }
+
+
+  datePickerValueChange(event) {
+    if (event?.target && new Date(event.target.valueAsDate) > this.getToday()) {
+      event.target.value = this.getToday();
+      setTimeout(() => (this.date_of_birth = this.getToday()), 2000);
+
+      this.date_of_birth = this.getToday();
+      this.form.patchValue({
+        date_of_birth: moment(this.date_of_birth).toISOString(),
+      });
+      return;
+    }
+        if (
+          (event.target.valueAsNumber === 0 ||
+            isNaN(event.target.valueAsNumber))
+        ) {
+          this.date_of_birth = null;
+          this.form.patchValue({
+            date_of_birth: null,
+          });
+          console.log('Setting DOB to null as it was reset');
+        }
+        if (event.target.valueAsNumber > 0) {
+          this.date_of_birth = new Date(event.target.valueAsDate);
+          this.form.patchValue({
+            date_of_birth: moment(event.target.valueAsDate).toISOString(),
+          });
+        }
   }
 }
---
+++ src/app/providers/region.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {StorageService} from '../core/services/storage.service';
+import {Observable} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {EMPTY} from 'rxjs/internal/observable/empty';
+import {Region} from '../model/region';
+
+
+@Injectable({ providedIn: 'root' })
+export class RegionService {
+  public regions: Region[];
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private storageService: StorageService
+  ) { }
+
+  public get regionsValue(): Region[] {
+    return this.regions;
+  }
+
+  public set regionsValue(regions: Region[]) {
+    this.regions = regions;
+  }
+
+  public getRegion(id): Region {
+    return this.regions.find(region => region.id === id);
+  }
+
+  public getRegionsReq(country_id: number): Observable<Region[]> {
+    return this.http.get<{places: any}>(`${environment.apiUrl}/regionsbycountry/${country_id}`).pipe(
+      map(data => {
+        this.regions = data.places.map(region => {
+          return {
+            id: region.pk,
+            name: region.val
+          };
+        });
+        debugger;
+        return this.regions;
+      }),
+      catchError((err, caught) => {
+        console.log(err);
+        return EMPTY;
+      })
+    );
+  }
+
+}
